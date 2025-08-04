cfd934d00568ad3e9f75f3f1aeae23798db09ff6
gig74 <gig74_tmp@mail.ru>
2025-08-04 13:27:27 +0500

    01 - acceptance version

--- src/main/java/ru/productstar/testrunners/exceptions/MovingFragileFarawayException.java
+++ src/main/java/ru/productstar/testrunners/exceptions/MovingFragileFarawayException.java
@@ -1,8 +1,6 @@
 package ru.productstar.testrunners.exceptions;
 
 public class MovingFragileFarawayException extends RuntimeException{
-    public MovingFragileFarawayException() {
-    }
     public MovingFragileFarawayException(String message) {
         super(message);
     }
--- src/test/java/ru/productstar/testrunners/TransportationTest.java
+++ src/test/java/ru/productstar/testrunners/TransportationTest.java
@@ -4,7 +4,15 @@ package ru.productstar.testrunners;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.support.ParameterDeclarations;
 import ru.productstar.testrunners.exceptions.MovingFragileFarawayException;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,44 +31,40 @@ class TransportationTest {
         );
     }
 
-    @Test
     @Tag("Smoke")
-    @ParametrizedTest
+    @ParameterizedTest
+    @ArgumentsSource(CalcPriceArgumentsProvider.class)
     @DisplayName("Расчеты стоимости тестовые")
-    void calcPriceTransportation() {
-        Transportation transportation01 = new Transportation(1, TransitServiceWorkload.NORMAL, false, false);
-        Transportation transportation02 = new Transportation(25, TransitServiceWorkload.LITTLEHIGH, false, false);
-        Transportation transportation03 = new Transportation(10, TransitServiceWorkload.LITTLEHIGH, true, false);
-        Transportation transportation04 = new Transportation(30, TransitServiceWorkload.VERYHIGH, true, true);
-        Transportation transportation05 = new Transportation(2147483647, TransitServiceWorkload.MEDIUMHIGH, true, false);
-        float price01 = 150;
-        float price02 = 360;
-        float price03 = 360;
-        float price04 = 1120;
-        float price05 = 700;
-        assertAll(
-                () -> assertEquals(price01, Transportation.getCurrentPrice(transportation01), "Неверно посчитана цена" + " для " + transportation01.toString() + " Расчетная цена " + Transportation.getCurrentPrice(transportation01)),
-                () -> assertEquals(price02, Transportation.getCurrentPrice(transportation02), "Неверно посчитана цена" + " для " + transportation02.toString() + " Расчетная цена " + Transportation.getCurrentPrice(transportation02)),
-                () -> assertEquals(price03, Transportation.getCurrentPrice(transportation03), "Неверно посчитана цена" + " для " + transportation03.toString() + " Расчетная цена " + Transportation.getCurrentPrice(transportation03)),
-                () -> assertEquals(price04, Transportation.getCurrentPrice(transportation04), "Неверно посчитана цена" + " для " + transportation04.toString() + " Расчетная цена " + Transportation.getCurrentPrice(transportation04)),
-                () -> assertEquals(price05, Transportation.getCurrentPrice(transportation05), "Неверно посчитана цена" + " для " + transportation05.toString() + " Расчетная цена " + Transportation.getCurrentPrice(transportation05))
-        );
+    void calcPriceTransportation(Transportation transportation, float price) {
+        assertEquals(price, Transportation.getCurrentPrice(transportation), "Неверно посчитана цена" + " для " + transportation.toString() + " Расчетная цена " + Transportation.getCurrentPrice(transportation));
     }
 
     @Test
     @Tag("Negative")
     @DisplayName("Некорректно указано расстояние перевозки")
     void illegalDistance() {
-        assertThrows(IllegalArgumentException.class, () -> new  Transportation(0, TransitServiceWorkload.NORMAL, false, false) ) ;
-        assertThrows(RuntimeException.class, () -> new  Transportation(-1 , TransitServiceWorkload.NORMAL, false, false) ) ; // Проверка, что IllegalArgumentException унаследован от RuntimeException (на всякий случай)
+        assertThrows(IllegalArgumentException.class, () -> new Transportation(0, TransitServiceWorkload.NORMAL, false, false));
+        assertThrows(RuntimeException.class, () -> new Transportation(-1, TransitServiceWorkload.NORMAL, false, false)); // Проверка, что IllegalArgumentException унаследован от RuntimeException (на всякий случай)
     }
 
     @Test
     @Tag("Negative")
     @DisplayName("Хрупкую вещь слишком далеко везут")
     void movingFragileFaraway() {
-        assertThrows(MovingFragileFarawayException.class, () -> new  Transportation(31, TransitServiceWorkload.NORMAL, false, true) ) ;
-        assertThrows(RuntimeException.class, () -> new  Transportation(31 , TransitServiceWorkload.NORMAL, false, true) ) ; // Проверка, что IllegalArgumentException унаследован от MovingFragileFarawayException (на всякий случай)
+        assertThrows(MovingFragileFarawayException.class, () -> new Transportation(31, TransitServiceWorkload.NORMAL, false, true));
+        assertThrows(RuntimeException.class, () -> new Transportation(31, TransitServiceWorkload.NORMAL, false, true)); // Проверка, что IllegalArgumentException унаследован от MovingFragileFarawayException (на всякий случай)
     }
 
+    static class CalcPriceArgumentsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                    Arguments.of(new Transportation(1, TransitServiceWorkload.NORMAL, false, false), 150f),
+                    Arguments.of(new Transportation(25, TransitServiceWorkload.LITTLEHIGH, false, false), 360f),
+                    Arguments.of(new Transportation(10, TransitServiceWorkload.LITTLEHIGH, true, false), 360f),
+                    Arguments.of(new Transportation(30, TransitServiceWorkload.VERYHIGH, true, true), 1120f),
+                    Arguments.of(new Transportation(2147483647, TransitServiceWorkload.MEDIUMHIGH, true, false), 700f)
+            );
+        }
+    }
 }
